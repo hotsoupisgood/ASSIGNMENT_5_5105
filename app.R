4bc8c79e0eb62e8d9c9d34d5b2323715e5cbd289
hotsoupisgood <lucasalexanderhenry@gmail.com>
2025-12-01 13:54:21 +0000

    add one plot, configure bmi and age sliders

--- app.R
+++ app.R
@@ -3,6 +3,9 @@ library(ggplot2)
 library(shinydashboard)
 library(dplyr)
 
+# load dataset globally
+dig.df=read.csv("DIG.csv") # this is not good practice, will reconfigure if I have time
+
 ui <- dashboardPage(
   # change the background
   skin = "green",
@@ -13,8 +16,12 @@ ui <- dashboardPage(
   dashboardSidebar(
     selectInput(inputId = "cvd",  label = "Filter on CVD Status:", choices = c("CVD", "No CVD", "All"), selected="All"),
     selectInput(inputId = "sex", label = "Select Subject Sex:",    choices = c("FEMALE", "MALE"), multiple=T, selected=c("FEMALE","MALE")),
-    sliderInput("bmass", "Select Body Mass Range:",  min = 0, max = 25,   value = c(0, 25)),
-    sliderInput("age", "Select Age of the Subject:", min = 15, max = 100, value = c(15, 100))
+    sliderInput("bmi", "Select Body Mass Range:",    
+                min =     min(dig.df$BMI,na.rm=T),  max=max(dig.df$BMI,na.rm=T),# range
+                value = c(min(dig.df$BMI,na.rm=T),  max(dig.df$BMI,na.rm=T))), # defaults 
+    sliderInput("age", "Select Age of the Subject:", 
+                min=min(dig.df$AGE,na.rm=T), max=max(dig.df$AGE,na.rm=T),    
+                value = c(min(dig.df$AGE,na.rm=T), max(dig.df$AGE,na.rm=T)))
   ),
   # tabs with plots
   dashboardBody(
@@ -30,7 +37,7 @@ ui <- dashboardPage(
            tabPanel("Hospitalisation", 
                     fluidRow(
                       box(width=12, title = "Hospitalisation", collapsible = F, status = "warning", solidHeader = TRUE,
-                          dataTableOutput("plot1")) 
+                          plotOutput("plot1")) 
                     )
                     
            ),
@@ -55,23 +62,34 @@ ui <- dashboardPage(
 
 # app logic
 server <- function(input, output) {
-  # load dataset
-  dig.df=read.csv("DIG.csv")
   # global filtering
   dig.df=dig.df %>% select(ID, TRTMT, AGE, SEX, BMI, DIABP, SYSBP, HYPERTEN, CVD, WHF, HOSP,DEATH, DEATHDAY)
   # filter change event handler
   dig_filtered_df <- reactive({
+    # make copy to not change global in case we need the global df
     df_copy=dig.df
+    # gender handler
     mapping_sex=c("FEMALE"=2,"MALE"=1)
     df_copy=df_copy%>%filter(SEX %in% mapping_sex[input$sex])
+    # age handler (inclusive filter)
+    df_copy=df_copy%>%filter(AGE>=input$age[1] & AGE<=input$age[2])
+    # bmi handler (inclusive filter)
+    df_copy=df_copy%>%filter(BMI>=input$bmi[1] & BMI<=input$bmi[2])
     return(df_copy)
   })
   # display mapping
   
-  #output$plot1 <- renderPlot({ 
+  output$plot1 <- renderPlot({ 
     #ggplot(data = dig_sub(), aes(x = AGE, y = BMI)) +
-    #  geom_point()
-  #})
+    #Hospitalization
+    ggplot(data=dig_filtered_df(), aes(x=CVD, fill = factor(TRTMT))) + 
+      labs(x="Treatment",
+           title="Mortality between treatment groups") +
+      scale_fill_manual(values = c("#669933", "#FFCC66"),
+                        name = "Treatment group") +
+      geom_bar(alpha=0.7, position="fill") +
+      labs(x="Hospitalization for CVD", title = "Hospitalization in treatment and mortality groups")
+  })
  
   
   output$table1=renderDataTable({
